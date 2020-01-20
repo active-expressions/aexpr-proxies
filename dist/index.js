6059e874bdfa9c7f745c1a9e595fc9a4b9c55e3d
jonasnoki <jonasnoackgruen@gmail.com>
2020-01-20 11:01:42 +0100

    Fix window vs global

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 exports.ProxiesActiveExpression = undefined;
 
@@ -22,134 +22,135 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-global.__expressionAnalysisMode__ = false;
+var globalOrWindow = typeof window == "undefined" ? global : window;
 
-global.__currentActiveExpression__ = false;
+globalOrWindow.__expressionAnalysisMode__ = false;
+
+globalOrWindow.__currentActiveExpression__ = false;
 
 function reset() {
-  // maps from target ids to active expressions
-  global.__proxyIdToActiveExpressionsMap__ = new Map();
+    // maps from target ids to active expressions
+    globalOrWindow.__proxyIdToActiveExpressionsMap__ = new Map();
 
-  // maps from proxy to target
-  global.__proxyToTargetMap__ = new WeakMap();
+    // maps from proxy to target
+    globalOrWindow.__proxyToTargetMap__ = new WeakMap();
 }
 
 reset();
 
 var basicHandlerFactory = function basicHandlerFactory(id) {
-  return {
-    get: function get(target, property) {
-      if (global.__expressionAnalysisMode__) {
-        var dependencies = global.__proxyIdToActiveExpressionsMap__.get(id);
+    return {
+        get: function get(target, property) {
+            if (globalOrWindow.__expressionAnalysisMode__) {
+                var dependencies = globalOrWindow.__proxyIdToActiveExpressionsMap__.get(id);
 
-        dependencies.add(global.__currentActiveExpression__);
-        global.__proxyIdToActiveExpressionsMap__.set(id, dependencies);
-      }
-      if (typeof target[property] === 'function') {
-        return Reflect.get(target, property).bind(target);
-      }
-      return Reflect.get(target, property);
-    },
+                dependencies.add(globalOrWindow.__currentActiveExpression__);
+                globalOrWindow.__proxyIdToActiveExpressionsMap__.set(id, dependencies);
+            }
+            if (typeof target[property] === 'function') {
+                return Reflect.get(target, property).bind(target);
+            }
+            return Reflect.get(target, property);
+        },
 
-    set: function set(target, property, value) {
-      Reflect.set(target, property, value);
+        set: function set(target, property, value) {
+            Reflect.set(target, property, value);
 
-      global.__proxyIdToActiveExpressionsMap__.get(id).forEach(function (dependentActiveExpression) {
-        return dependentActiveExpression.notifyOfUpdate();
-      });
-      return true;
-    }
-  };
+            globalOrWindow.__proxyIdToActiveExpressionsMap__.get(id).forEach(function (dependentActiveExpression) {
+                return dependentActiveExpression.notifyOfUpdate();
+            });
+            return true;
+        }
+    };
 };
 
 var functionHandlerFactory = function functionHandlerFactory(id) {
-  return {
-    apply: function apply(target, thisArg, argumentsList) {
-      thisArg = global.__proxyToTargetMap__.get(thisArg) || thisArg;
+    return {
+        apply: function apply(target, thisArg, argumentsList) {
+            thisArg = globalOrWindow.__proxyToTargetMap__.get(thisArg) || thisArg;
 
-      if (global.__expressionAnalysisMode__) {
-        return target.bind(thisArg).apply(undefined, _toConsumableArray(argumentsList));
-      }
-      var result = target.bind(thisArg).apply(undefined, _toConsumableArray(argumentsList));
-      global.__proxyIdToActiveExpressionsMap__.get(id).forEach(function (dependentActiveExpression) {
-        return dependentActiveExpression.notifyOfUpdate();
-      });
-      return result;
-    }
-  };
+            if (globalOrWindow.__expressionAnalysisMode__) {
+                return target.bind(thisArg).apply(undefined, _toConsumableArray(argumentsList));
+            }
+            var result = target.bind(thisArg).apply(undefined, _toConsumableArray(argumentsList));
+            globalOrWindow.__proxyIdToActiveExpressionsMap__.get(id).forEach(function (dependentActiveExpression) {
+                return dependentActiveExpression.notifyOfUpdate();
+            });
+            return result;
+        }
+    };
 };
 
 function unwrap(proxy) {
-  return global.__proxyToTargetMap__.get(proxy) || proxy;
+    return globalOrWindow.__proxyToTargetMap__.get(proxy) || proxy;
 }
 
 function wrap(typeOfWhat, what) {
-  if (global.__proxyToTargetMap__.has(what)) return what;
-  var id = global.__proxyIdToActiveExpressionsMap__.size;
-  var basicHandler = basicHandlerFactory(id);
-
-  if (typeOfWhat !== 'Object') {
+    if (globalOrWindow.__proxyToTargetMap__.has(what)) return what;
+    var id = globalOrWindow.__proxyIdToActiveExpressionsMap__.size;
+    var basicHandler = basicHandlerFactory(id);
 
     var prototypes = {
-      "Set": Set.prototype,
-      "Map": Map.prototype,
-      "Array": Array.prototype
+        "Set": Set.prototype,
+        "Map": Map.prototype,
+        "Array": Array.prototype
     };
 
-    var functionHandler = functionHandlerFactory(id);
-    var methods = Object.getOwnPropertyNames(prototypes[typeOfWhat]).filter(function (propName) {
-      return typeof what[propName] === 'function';
-    });
-    methods.forEach(function (method) {
-      return what[method] = new Proxy(what[method], functionHandler);
-    });
-  }
+    if (prototypes[typeOfWhat]) {
+        var functionHandler = functionHandlerFactory(id);
+        var methods = Object.getOwnPropertyNames(prototypes[typeOfWhat]).filter(function (propName) {
+            return typeof what[propName] === 'function';
+        });
+        methods.forEach(function (method) {
+            return what[method] = new Proxy(what[method], functionHandler);
+        });
+    }
 
-  global.__proxyIdToActiveExpressionsMap__.set(id, new Set());
-  var proxy = new Proxy(what, basicHandler);
-  global.__proxyToTargetMap__.set(proxy, what);
+    globalOrWindow.__proxyIdToActiveExpressionsMap__.set(id, new Set());
+    var proxy = new Proxy(what, basicHandler);
+    globalOrWindow.__proxyToTargetMap__.set(proxy, what);
 
-  return proxy;
+    return proxy;
 }
 
 function aexpr(func) {
-  for (var _len = arguments.length, arg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    arg[_key - 1] = arguments[_key];
-  }
+    for (var _len = arguments.length, arg = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+        arg[_key - 1] = arguments[_key];
+    }
 
-  return new (Function.prototype.bind.apply(ProxiesActiveExpression, [null].concat([func], arg)))();
+    return new (Function.prototype.bind.apply(ProxiesActiveExpression, [null].concat([func], arg)))();
 }
 
 var ProxiesActiveExpression = exports.ProxiesActiveExpression = function (_BaseActiveExpression) {
-  _inherits(ProxiesActiveExpression, _BaseActiveExpression);
+    _inherits(ProxiesActiveExpression, _BaseActiveExpression);
 
-  function ProxiesActiveExpression(func) {
-    var _ref;
+    function ProxiesActiveExpression(func) {
+        var _ref;
 
-    _classCallCheck(this, ProxiesActiveExpression);
+        _classCallCheck(this, ProxiesActiveExpression);
 
-    for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-      args[_key2 - 1] = arguments[_key2];
+        for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+            args[_key2 - 1] = arguments[_key2];
+        }
+
+        var _this = _possibleConstructorReturn(this, (_ref = ProxiesActiveExpression.__proto__ || Object.getPrototypeOf(ProxiesActiveExpression)).call.apply(_ref, [this, func].concat(args)));
+
+        _this.notifyOfUpdate();
+        return _this;
     }
 
-    var _this = _possibleConstructorReturn(this, (_ref = ProxiesActiveExpression.__proto__ || Object.getPrototypeOf(ProxiesActiveExpression)).call.apply(_ref, [this, func].concat(args)));
+    _createClass(ProxiesActiveExpression, [{
+        key: 'notifyOfUpdate',
+        value: function notifyOfUpdate() {
+            globalOrWindow.__expressionAnalysisMode__ = true;
+            globalOrWindow.__currentActiveExpression__ = this;
 
-    _this.notifyOfUpdate();
-    return _this;
-  }
+            this.func();
+            this.checkAndNotify();
 
-  _createClass(ProxiesActiveExpression, [{
-    key: 'notifyOfUpdate',
-    value: function notifyOfUpdate() {
-      global.__expressionAnalysisMode__ = true;
-      global.__currentActiveExpression__ = this;
+            globalOrWindow.__expressionAnalysisMode__ = false;
+        }
+    }]);
 
-      this.func();
-      this.checkAndNotify();
-
-      global.__expressionAnalysisMode__ = false;
-    }
-  }]);
-
-  return ProxiesActiveExpression;
+    return ProxiesActiveExpression;
 }(_activeExpressions.BaseActiveExpression);
--- index.js
+++ index.js
@@ -1,113 +1,114 @@
 import { BaseActiveExpression } from 'active-expressions';
 
-global.__expressionAnalysisMode__ = false;
+const globalOrWindow = typeof window == "undefined" ? global : window;
 
-global.__currentActiveExpression__ = false;
+globalOrWindow.__expressionAnalysisMode__ = false;
+
+globalOrWindow.__currentActiveExpression__ = false;
 
 export function reset() {
-  // maps from target ids to active expressions
-  global.__proxyIdToActiveExpressionsMap__ = new Map();
+    // maps from target ids to active expressions
+    globalOrWindow.__proxyIdToActiveExpressionsMap__ = new Map();
 
-  // maps from proxy to target
-  global.__proxyToTargetMap__ = new WeakMap();
+    // maps from proxy to target
+    globalOrWindow.__proxyToTargetMap__ = new WeakMap();
 }
 
 reset();
 
 const basicHandlerFactory = id => ({
-  get: (target, property) => {
-    if (global.__expressionAnalysisMode__) {
-      let dependencies = global.__proxyIdToActiveExpressionsMap__.get(id);
+    get: (target, property) => {
+        if (globalOrWindow.__expressionAnalysisMode__) {
+            let dependencies = globalOrWindow.__proxyIdToActiveExpressionsMap__.get(id);
 
-      dependencies.add(global.__currentActiveExpression__);
-      global.__proxyIdToActiveExpressionsMap__.set(id, dependencies);
-    }
-    if (typeof target[property] === 'function') {
-      return Reflect.get(target, property).bind(target);
-    }
-    return Reflect.get(target, property);
-  },
+            dependencies.add(globalOrWindow.__currentActiveExpression__);
+            globalOrWindow.__proxyIdToActiveExpressionsMap__.set(id, dependencies);
+        }
+        if (typeof target[property] === 'function') {
+            return Reflect.get(target, property).bind(target);
+        }
+        return Reflect.get(target, property);
+    },
 
-  set: (target, property, value) => {
-    Reflect.set(target, property, value);
+    set: (target, property, value) => {
+        Reflect.set(target, property, value);
 
-    global.__proxyIdToActiveExpressionsMap__
-      .get(id)
-      .forEach(dependentActiveExpression =>
-        dependentActiveExpression.notifyOfUpdate()
-      );
-    return true;
-  },
+        globalOrWindow.__proxyIdToActiveExpressionsMap__
+            .get(id)
+            .forEach(dependentActiveExpression =>
+                dependentActiveExpression.notifyOfUpdate()
+            );
+        return true;
+    },
 });
 
 const functionHandlerFactory = id => ({
-  apply: (target, thisArg, argumentsList) => {
-    thisArg = global.__proxyToTargetMap__.get(thisArg) || thisArg;
+    apply: (target, thisArg, argumentsList) => {
+        thisArg = globalOrWindow.__proxyToTargetMap__.get(thisArg) || thisArg;
 
-    if (global.__expressionAnalysisMode__) {
-      return target.bind(thisArg)(...argumentsList);
-    }
-    const result = target.bind(thisArg)(...argumentsList);
-    global.__proxyIdToActiveExpressionsMap__
-      .get(id)
-      .forEach(dependentActiveExpression =>
-        dependentActiveExpression.notifyOfUpdate()
-      );
-    return result;
-  },
+        if (globalOrWindow.__expressionAnalysisMode__) {
+            return target.bind(thisArg)(...argumentsList);
+        }
+        const result = target.bind(thisArg)(...argumentsList);
+        globalOrWindow.__proxyIdToActiveExpressionsMap__
+            .get(id)
+            .forEach(dependentActiveExpression =>
+                dependentActiveExpression.notifyOfUpdate()
+            );
+        return result;
+    },
 });
 
 export function unwrap(proxy) {
-  return global.__proxyToTargetMap__.get(proxy) || proxy;
+    return globalOrWindow.__proxyToTargetMap__.get(proxy) || proxy;
 }
 
 export function wrap(typeOfWhat, what) {
-  if (global.__proxyToTargetMap__.has(what)) return what;
-  const id = global.__proxyIdToActiveExpressionsMap__.size;
-  const basicHandler = basicHandlerFactory(id);
-
-  if (typeOfWhat !== 'Object') {
-    
-    const prototypes = {
-      "Set": Set.prototype,
-      "Map": Map.prototype,
-      "Array": Array.prototype
+    if (globalOrWindow.__proxyToTargetMap__.has(what)) return what;
+    const id = globalOrWindow.__proxyIdToActiveExpressionsMap__.size;
+    const basicHandler = basicHandlerFactory(id);
+  
+      const prototypes = {
+          "Set": Set.prototype,
+          "Map": Map.prototype,
+          "Array": Array.prototype
+      };
+  
+    if (prototypes[typeOfWhat]) {
+      const functionHandler = functionHandlerFactory(id);
+      const methods = Object.getOwnPropertyNames(prototypes[typeOfWhat]).filter(
+        propName => typeof what[propName] === 'function'
+      );
+      methods.forEach(
+        method => (what[method] = new Proxy(what[method], functionHandler))
+      );
     }
-    
-    const functionHandler = functionHandlerFactory(id);
-    const methods = Object.getOwnPropertyNames(prototypes[typeOfWhat]).filter(
-      propName => typeof what[propName] === 'function'
-    );
-    methods.forEach(
-      method => (what[method] = new Proxy(what[method], functionHandler))
-    );
-  }
-
-  global.__proxyIdToActiveExpressionsMap__.set(id, new Set());
-  const proxy = new Proxy(what, basicHandler);
-  global.__proxyToTargetMap__.set(proxy, what);
-
-  return proxy;
+  
+    globalOrWindow.__proxyIdToActiveExpressionsMap__.set(id, new Set());
+    const proxy = new Proxy(what, basicHandler);
+    globalOrWindow.__proxyToTargetMap__.set(proxy, what);
+  
+    return proxy;
 }
 
 
 export function aexpr(func, ...arg) {
-  return new ProxiesActiveExpression(func, ...arg);
+    return new ProxiesActiveExpression(func, ...arg);
 }
 
 export class ProxiesActiveExpression extends BaseActiveExpression {
-  constructor(func, ...args) {
-    super(func, ...args);
-    this.notifyOfUpdate();
-  }
+    constructor(func, ...args) {
+        super(func, ...args);
+        this.notifyOfUpdate();
+    }
 
-  notifyOfUpdate() {
-    global.__expressionAnalysisMode__ = true;
-    global.__currentActiveExpression__ = this;
+    notifyOfUpdate() {
+        globalOrWindow.__expressionAnalysisMode__ = true;
+        globalOrWindow.__currentActiveExpression__ = this;
 
-    this.func();
-    this.checkAndNotify();
+        this.func();
+        this.checkAndNotify();
 
-    global.__expressionAnalysisMode__ = false;
-  }
+        globalOrWindow.__expressionAnalysisMode__ = false;
+    }
 }
